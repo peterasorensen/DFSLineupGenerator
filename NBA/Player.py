060247a2d06d0cce82b9a22c598cff67694fce33
Peter Sorensen <peter.sorensen@berkeley.edu>
2017-11-12 15:30:03 -0800

    refactoring

---
+++ NBA/Player.py
@@ -0,0 +1,8 @@
+class Player:
+
+    def __init__(self, name):
+        playerName = name
+        playerID = "f"
+
+    def somthing(self):
+        print("f")
---
+++ NBA/PlayerIDMapping.py
@@ -0,0 +1,21 @@
+import json
+
+def mapGen():
+    """Run only to re-generate NameIDMapping.json"""
+    with open('../Players.json','r') as f:
+        players_dict = json.load(f)
+        playerDict = {}
+        for player in players_dict['resultSets'][0]['rowSet']:
+            playerDict[player[2]] = player[0]
+        output = open('../NameIDMapping.json', 'w')
+        output.write(json.dumps(playerDict))
+
+def mapAccess(playerName):
+    """Retrieve PlayerID from NameIDMapping.json
+    using players full name.
+    mapAccess('Kevin Durant') --> 201142"""
+    with open('../NameIDMapping.json', 'r') as f:
+        mapping = json.load(f)
+        if playerName.title() not in mapping:
+            raise ValueError(playerName + " is not a player that exists.")
+        return mapping[playerName.title()]
